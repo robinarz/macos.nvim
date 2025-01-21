af162fb4fbf00572402a37cfaf4117cdbad69404
Robinho <robin.arzinierebaffert@gmail.com>
2025-01-21 23:02:10 +0100

    feat(completion): testing supermaven

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -10,6 +10,7 @@ return {
       "hrsh7th/cmp-buffer",
       { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
       "saadparwaiz1/cmp_luasnip",
+      "roobert/tailwindcss-colorizer-cmp.nvim",
     },
     config = function()
       require "custom.completion"
